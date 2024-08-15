c7d78b0411a9fd89d07975f233bf1e0883404bf1
Chukwuneku <akpotohwoo@gmail.com>
2024-08-15 01:57:58 +0100

    add task 2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,55 @@
+interface DirectorInterface {
+    workFromHome(): string
+
+    getCoffeeBreak(): string
+
+    workDirectorTasks(): string
+}
+
+interface TeacherInterface {
+    workFromHome(): string
+
+    getCoffeeBreak(): string
+
+    workTeacherTasks(): string
+}
+
+export class Director implements DirectorInterface {
+
+    workFromHome = (): string => "Working from home";
+
+    getCoffeeBreak = (): string => "Getting a coffee break";
+
+    workDirectorTasks = (): string => "Getting to director tasks";
+
+}
+
+export class Teacher implements TeacherInterface {
+
+    workFromHome = (): string => "Cannot work from home";
+
+    getCoffeeBreak = (): string => "Cannot have a break";
+
+    workTeacherTasks = (): string => "Getting to work";
+
+}
+
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
+
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+    return (employee as Director).workDirectorTasks() !== undefined;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    let res = undefined;
+    (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
+    return res;
+}
+
+type Subjects = "Math" | "History";
+
+// @ts-ignore
+// @ts-ignore
+export function teachClass(todayClass: Subjects): string | boolean {
+    return (todayClass === "Math") ? "Teaching Math" : (todayClass === "History") ? "Teaching History" : `${undefined}`
+}
