7cc5b2503f07a05e5cc284d2d65fe9742f40396a
Chukwuneku <akpotohwoo@gmail.com>
2024-10-05 01:38:22 +0100

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays message on STDOUT.
+ * @param {String} msg The message to display.
+ * @author Chukwuneku Akpotohwo <https://github.com/nekumartins>>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
